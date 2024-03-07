46622d86d4e992a674e8b743e6a330dcb418c5f8
vwkd <33468089+vwkd@users.noreply.github.com>
2024-03-08 01:16:49 +0400

    data/de: use official data

--- src/data/de.ts
+++ src/data/de.ts
@@ -1,48 +1,48 @@
 /**
  * Yearly inflation rate in Germany in percent
  *
- * - until 1991 using [Privatschule Eberhard - Preisindex für Lebenshaltung seit 1881](https://web.archive.org/web/20160311213037/http://privatschule-eberhard.de/index.php/wissenswertes/8-preissteigerungsraten)
- * - note: i-th value computed from index with $i/(i-1)$, from column "1881 = 100" since more precision than column "2005 = 100"
- * - note: data possibly not very accurate, up to 1% difference to VPI in overlapping region
- * - from 1992 using [Destatis - Tabelle 61111-0001: Verbraucherpreisindex: Deutschland, Jahre](https://www-genesis.destatis.de/genesis//online?operation=table&code=61111-0001&bypass=true&levelindex=0&levelid=1708435341978#abreadcrumb)
+ * - from 1958 until 1962 using "Preisindex für die Lebenshaltung aller 2-Personen-Rentner-Haushalte mit geringem Einkommen im früheren Bundesgebiet" from [Destatis - Statistischer Bericht - Verbraucherpreisindex für Deutschland - Lange Reihen ab 1948 - Januar 2024](https://www.destatis.de/DE/Themen/Wirtschaft/Preise/Verbraucherpreisindex/Publikationen/Downloads-Verbraucherpreise/statistischer-bericht-verbraucherpreisindex-lange-reihen-5611103241015.html)
+ * - from 1963 until 1991 using "Preisindex für die Lebenshaltung aller privaten Haushalte im früheren Bundesgebiet" from [Destatis - Statistischer Bericht - Verbraucherpreisindex für Deutschland - Lange Reihen ab 1948 - Januar 2024](https://www.destatis.de/DE/Themen/Wirtschaft/Preise/Verbraucherpreisindex/Publikationen/Downloads-Verbraucherpreise/statistischer-bericht-verbraucherpreisindex-lange-reihen-5611103241015.html)
+ * - from 1992 using "Verbraucherpreisindex für Deutschland" from [Destatis - Tabelle 61111-0001: Verbraucherpreisindex: Deutschland, Jahre](https://www-genesis.destatis.de/genesis//online?operation=table&code=61111-0001&bypass=true&levelindex=0&levelid=1708435341978#abreadcrumb)
  */
 export const inflationRates = {
-  // Privatschule Eberhard
-  1958: 1.9,
-  1959: 1.1,
-  1960: 1.5,
-  1961: 2.2,
-  1962: 2.9,
-  1963: 3.1,
+  // Preisindex für die Lebenshaltung aller 2-Personen-Rentner-Haushalte mit geringem Einkommen im früheren Bundesgebiet
+  1958: 2.1,
+  1959: 1.0,
+  1960: 1.3,
+  1961: 2.7,
+  1962: 3.2,
+  // Preisindex für die Lebenshaltung aller privaten Haushalte im früheren Bundesgebiet
+  1963: 3,
   1964: 2.4,
-  1965: 3.0,
-  1966: 3.8,
-  1967: 1.6,
-  1968: 1.5,
-  1969: 2.1,
-  1970: 3.2,
-  1971: 5.4,
+  1965: 3.2,
+  1966: 3.3,
+  1967: 1.9,
+  1968: 1.6,
+  1969: 1.8,
+  1970: 3.6,
+  1971: 5.2,
   1972: 5.4,
   1973: 7.1,
   1974: 6.9,
-  1975: 5.8,
-  1976: 4.4,
-  1977: 3.6,
+  1975: 6,
+  1976: 4.2,
+  1977: 3.7,
   1978: 2.7,
-  1979: 4.0,
-  1980: 5.5,
-  1981: 6.4,
+  1979: 4.1,
+  1980: 5.4,
+  1981: 6.3,
   1982: 5.2,
   1983: 3.2,
-  1984: 2.4,
-  1985: 2.2,
+  1984: 2.5,
+  1985: 2,
   1986: -0.1,
-  1987: 0.3,
-  1988: 1.1,
+  1987: 0.2,
+  1988: 1.2,
   1989: 2.8,
-  1990: 2.8,
-  1991: 3.6,
-  // Verbraucherpreisindex
+  1990: 2.6,
+  1991: 3.7,
+  // Verbraucherpreisindex für Deutschland
   1992: 5.0,
   1993: 4.5,
   1994: 2.7,
